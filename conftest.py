4a4ab25f144d9439b1afde74177d534f87cdd807
prakashvbhat <148607242+prakashvbhat@users.noreply.github.com>
2024-07-19 11:14:33 +0530

    frist file
    frisr

--- conftest.py
+++ conftest.py
@@ -20,7 +20,7 @@ import signal
 from types import FrameType
 from typing import Optional
 
-import numpy as np
+import "C:\Users\praveen v\Documents\GitHub\corenet\conftest.py", as np
 import pytest
 import torch
 
